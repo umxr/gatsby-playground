76e3e64bf93f00693974e86f7a74176fd88538a2
umxr <umarg1997@gmail.com>
2019-07-06 17:49:49 +0100

    styling done

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Header from '../components/Header';
 
-export default () => (
-  <>
+const About = () => (
+  <Header>
     <h1>About Me</h1>
     <p>This is a p tag</p>
     <Link to="/">Home</Link>
-  </>
+  </Header>
 );
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Header from '../components/Header';
 
-export default () => {
-  return (
-    <>
-      <h1>Home</h1>
-      <Link to="/about">About Me</Link>
-    </>
-  )
-}
+const Index = () => (
+  <Header>
+    <h1>Home</h1>
+    <Link to="/about">About Me</Link>
+  </Header>
+);
+
+export default Index;
