58a4e45d9dcde27b451bf82619f5fbe7714685c5
Umar G <umar@statementagency.com>
2019-08-02 13:47:47 +0100

    wipe

--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Frontend Masters Gatsby Workshop',
-    description: 'A site built following a tutorial on Frontend masters',
-  },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
-};
