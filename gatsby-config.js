ce33735f3193f66d21de823af2da796df79dd037
Umar G <umar@statementagency.com>
2019-08-02 17:34:56 +0100

    Changes

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Frontend Masters Gatsby Workshop',
+    description:
+      'A site we built together during a full-day Frontend Masters Gatsby workshop!',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
+};
